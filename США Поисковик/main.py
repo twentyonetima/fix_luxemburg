d479264869506a8b610e34b4bb0e91d2b2915aa6
Тимофей Петров <tp@calculate.ru>
2023-12-07 18:04:56 +0300

    a part of fix США Поисковик

--- США Поисковик/main.py
+++ США Поисковик/main.py
@@ -1,26 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 import SaveHdd
 import WorkWebSite
+import time
 
-# поключаем driver
-# region parametr Brauser
+# region parameter Browser
 chrome_options = Options()
-# chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument("--no-sandbox") # linux only
-chrome_options.add_argument("--headless=new")  # for Chrome >= 109
-# chrome_options.add_argument("--headless")
-# chrome_options.headless = True # also works
-driver = webdriver.Chrome()  # режим браузера:  options true безгаловый
+chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome()
 url = "https://www.sec.gov/enforce/public-alerts"
 driver.get(url)
+
+# Wait for a few seconds to let the JavaScript load the content
+time.sleep(5)
+
+# Get the page source after JavaScript execution
+page_source = driver.page_source
+
+# Close the browser
+driver.quit()
 # endregion
 
-test = WorkWebSite.parsing(url, driver, "black_list")
+# Use BeautifulSoup for parsing the page source
+soup = BeautifulSoup(page_source, 'html.parser')
+
+# Call the parsing function with the URL, type_list, and soup
+test = WorkWebSite.parsing(url, "black_list", soup)
+
+# Save the result to a JSON file
 save = SaveHdd.save_json(test)
-
-
-
-
-
